060743a36486a2ddb2acda7bbc392422fe30912e
spaladu5 <paladugusaketh@gmail.com>
2024-11-20 10:59:35 -0500

    Initial commit with full project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+  export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9db8967b6dmsha8a88663ee4066dp109291jsnf9d7d54f6e63",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "f22d4a091c54883dfe97b2cf5397c28a"; // enter your key from openweather API
